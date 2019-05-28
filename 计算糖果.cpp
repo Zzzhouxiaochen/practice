4feed7f01966ee989c93b1cfc70516e0e2d74d60
Zzzhouxiaochen <zxc2890326540@163.com>
2019-05-28 15:45:19 +0800

    5.28

---
+++ 计算糖果.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int n1, n2, n3, n4;
+	int A, B, C;
+	cin >> n1 >> n2 >> n3 >> n4;
+	A = (n1 + n3) / 2;
+	B = A - n1;
+	C = B - n2;
+	if (A >= 0 && B >= 0 && C >= 0 && n4 == (B + C))
+		cout << A << " " << B << " " << C;
+	else
+		cout << "No";
+}
